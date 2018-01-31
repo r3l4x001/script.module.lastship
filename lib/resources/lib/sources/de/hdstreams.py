8fa0786f0ad6a67f2f517b2ccbca48c367ba045f
lastship <lastship@users.noreply.github.com>
2018-01-31 19:10:05 +0100

    Update hdstreams.py

--- lib/resources/lib/sources/de/hdstreams.py
+++ lib/resources/lib/sources/de/hdstreams.py
@@ -27,18 +27,12 @@ import hashlib
 
 
 from resources.lib.modules import pyaes
-#from resources.lib.modules import cache
-#from resources.lib.modules import cleantitle
-#from resources.lib.modules import client
-#from resources.lib.modules import source_utils
-#from resources.lib.modules import dom_parser
 from resources.lib.modules import cfscrape
 
 
 
 class source:
     def __init__(self):
-        print "print init hdstreams"
         self.priority = 1
         self.language = ['de']
         self.domains = ['hd-streams.org']
@@ -77,13 +71,12 @@ class source:
 
     def sources(self, url, hostDict, hostprDict):
         sources = []
-        print "print hdstreams def sources"
         try:
             if not url:
                 return sources
 
             sHtmlContent=self.scraper.get(url).content
-
+            
             pPattern = "loadStream[^>]'([^']+)', '([^']+)', '([^']+).*?"
             pPattern += '>.*?>([^"]+)</v-btn>'
             aResult = re.compile(pPattern, re.DOTALL).findall(sHtmlContent)
@@ -107,8 +100,8 @@ class source:
                         sources.append({'source': 'openload.com', 'quality': '1080p', 'language': 'de', 'url': link, 'direct': False, 'debridonly': False})
                     elif 'streamango' in link:
                         sources.append({'source': 'streamango.com', 'quality': 'HD', 'language': 'de', 'url': link, 'direct': False, 'debridonly': False})
-                    #elif 'nxload' in link:
-                        #sources.append({'source': 'nxload.com', 'quality': '1080p', 'language': 'de', 'url': link, 'direct': True, 'debridonly': False})
+                    elif 'nxload' in link:
+                        sources.append({'source': 'nxload.com', 'quality': '1080p', 'language': 'de', 'url': link, 'direct': False, 'debridonly': False})
                     elif 'streamcloud' in link:
                         sources.append({'source': 'streamcloud.com', 'quality': 'SD', 'language': 'de', 'url': link, 'direct': False, 'debridonly': False})
                 else:
@@ -116,8 +109,8 @@ class source:
                         sources.append({'source': 'openload.com', 'quality': 'HD', 'language': 'de', 'url': link, 'direct': False, 'debridonly': False})
                     elif 'streamango' in link:
                         sources.append({'source': 'streamango.com', 'quality': 'HD', 'language': 'de', 'url': link, 'direct': False, 'debridonly': False})
-                    #elif 'nxload' in link:
-                        #sources.append({'source': 'nxload.com', 'quality': 'HD', 'language': 'de', 'url': link, 'direct': True, 'debridonly': False})
+                    elif 'nxload' in link:
+                        sources.append({'source': 'nxload.com', 'quality': 'HD', 'language': 'de', 'url': link, 'direct': False, 'debridonly': False})
                     elif 'streamcloud' in link:
                         sources.append({'source': 'streamcloud.com', 'quality': 'SD', 'language': 'de', 'url': link, 'direct': False, 'debridonly': False})
 
@@ -147,42 +140,7 @@ class source:
             fUrl=sUrl2.replace('\/', '/').replace('"', '')       
                 
             return fUrl
-                   
-    def __evp_decode(self,cipher_text, passphrase, salt=None):
-        cipher_text = base64.b64decode(cipher_text)
-        if not salt:
-            salt = cipher_text[8:16]
-            cipher_text = cipher_text[16:]
-        data = self.__evpKDF(passphrase, salt)
-        decrypter = pyaes.Decrypter(pyaes.AESModeOfOperationCBC(data['key'], data['iv']))
-        plain_text = decrypter.feed(cipher_text)
-        plain_text += decrypter.feed()
-        return plain_text
 
-    
-    def __evpKDF(self,passwd, salt, key_size=8, iv_size=4, iterations=1, hash_algorithm="md5"):
-        target_key_size = key_size + iv_size
-        derived_bytes = ""
-        number_of_derived_words = 0
-        block = None
-        hasher = hashlib.new(hash_algorithm)
-        while number_of_derived_words < target_key_size:
-            if block is not None:
-                hasher.update(block)
-            hasher.update(passwd)
-            hasher.update(salt)
-            block = hasher.digest()
-            hasher = hashlib.new(hash_algorithm)
-            for _i in range(1, iterations):
-                hasher.update(block)
-                block = hasher.digest()
-                hasher = hashlib.new(hash_algorithm)
-            derived_bytes += block[0: min(len(block), (target_key_size - number_of_derived_words) * 4)]
-            number_of_derived_words += len(block) / 4
-        return {
-            "key": derived_bytes[0: key_size * 4],
-            "iv": derived_bytes[key_size * 4:]
-        }
 
 
     def resolve(self, url):
@@ -190,9 +148,18 @@ class source:
 
     def __search(self, imdb):
         try:
-            
+            sHtmlContent=self.scraper.get(self.base_link).content
+            #print "print hdstreams.org shtml entry", sHtmlContent
+            pattern = '<meta name="csrf-token" content="([^"]+)">'
+            string = str(sHtmlContent)
+            token = re.compile(pattern, flags=re.I | re.M).findall(string)
+            #print "print hdstreams.org shtml entry token", token[0]
             # first iteration of session object to be parsed for search
-            sHtmlContent=self.scraper.get(self.search % imdb).content
+            #sHtmlContent=self.scraper.get(self.search % imdb).content
+            #sHtmlContent=self.scraper.get(self.search % imdb).content
+            sHtmlContent=self.scraper.get(self.search % imdb,headers={'X-CSRF-TOKEN':token[0],'X-Requested-With':'XMLHttpRequest'}).text
+               
+            #print "print hdstreams.org shtml", sHtmlContent
             pattern = '"title":"([^"]+).*?"url":"([^"]+).*?src":"([^"]+)'
             aResult = re.compile(pattern, re.DOTALL).findall(sHtmlContent)
             
